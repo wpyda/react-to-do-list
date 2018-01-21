5fb05b1dbc1de48c113db1a08125694a208c6775
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-21 18:43:10 +0100

    adding to database

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -4,19 +4,46 @@ import {database} from './firebase'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
+import ActionDelete from 'material-ui/svg-icons/action/delete'
 import TextField from 'material-ui/TextField'
 import {List, ListItem} from 'material-ui/List';
 
+const Task = (props) => (
+
+    <ListItem
+        primaryText={props.taskName}
+        rightIcon={
+            <ActionDelete
+                onClick={() => props.deleteTask(props.taskId)}/>}
+    />
+)
+
 const style = {
-    margin: 20,
-    padding: 20,
+    margin: 25,
+    padding: 25,
     textAlign: 'center'
-};
+}
 
 class ToDoList extends React.Component {
 
     state = {
         text: '',
+        tasks: null
+    }
+
+    componentWillMount() {
+        database.ref('/ToDoList-Homework').on(
+            'value', (snapshot) => {
+                const tasksArray = Object.entries(
+                    snapshot.val() || {}
+                ).map(([key, value]) => {
+                    value.key = key
+                    return value
+                })
+
+                this.setState({tasks: tasksArray})
+            }
+        )
     }
 
     updateTask = (event, value) => {
@@ -35,10 +62,15 @@ class ToDoList extends React.Component {
         })
     }
 
+    deleteTask = (taskId) => {
+        database.ref(`/ToDoList-Homework/${taskId}`).remove()
+    }
+
     render() {
         return (
-            <MuiThemeProvider >
+            <MuiThemeProvider>
                 <Paper zDepth={4} style={style}>
+                    <h1>To Do List</h1>
 
                     <TextField
                         hintText="Type here"
@@ -50,17 +82,26 @@ class ToDoList extends React.Component {
 
                     <RaisedButton
                         label="Submit"
-                        secondary={true}
+                        primary={true}
                         fullWidth={true}
                         onClick={this.submitTask}
                     />
 
-                        <List>
-                            <ListItem
-                                primaryText="Wyrzucić śmieci"
-                            />
+                    <List style={{textAlign: 'left'}}>
+                        {
+                            this.state.tasks
+                            &&
+                            this.state.tasks.map((task) => (
+                                <Task
+                                    taskName={task.task}
+                                    taskId={task.key}
+                                    key={task.key}
+                                    deleteTask={this.deleteTask}
+                                />
+                            ))
+                        }
 
-                        </List>
+                    </List>
 
                 </Paper>
             </MuiThemeProvider>
